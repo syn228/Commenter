ea2b66397ea1cff945a7692095ada700b4a1eaf5
Aaron Na <syn228@nyu.edu>
2018-11-02 18:49:28 -0700

    made commentbox testing file and tried using full DOM mount function from enzyme

---
+++ components/__tests__/CommentBox.test.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import CommentBox from 'components/CommentBox'
+import { mount } from 'enzyme'
+
+let wrapped;
+
+beforeEach(() => {
+  wrapped = mount(<CommentBox />)
+});
+
+it('shows a text area and a button', () => {
+  expect(wrapped.find('textarea').length).toEqual(1)
+  expect(wrapped.find('button').length).toEqual(1)
+});
