8593377b84f63591e8fffcc824804cc80d21c0ce
Aaron Na <syn228@nyu.edu>
2018-11-05 14:37:07 -0800

    simulated change event and sent fake event object into the full DOM testing environment

--- components/__tests__/CommentBox.test.js
+++ components/__tests__/CommentBox.test.js
@@ -8,7 +8,16 @@ beforeEach(() => {
   wrapped = mount(<CommentBox />)
 });
 
+afterEach(() => {
+  wrapped.unmount()
+});
+
 it('shows a text area and a button', () => {
   expect(wrapped.find('textarea').length).toEqual(1)
   expect(wrapped.find('button').length).toEqual(1)
 });
+
+it('allows users to write in the text area', () => {
+  expect(wrapped.find('textarea').length).toEqual(1)
+});
+
