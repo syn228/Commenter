b300493d91741ccb1b8a8a4e6345a055c55dc5a6
Aaron Na <syn228@nyu.edu>
2018-11-07 16:09:58 -0800

    fetched dummy comment data with axios and redux-promise libraries

--- Root.js
+++ Root.js
@@ -1,11 +1,18 @@
 import { Provider } from 'react-redux'
-import React from 'react'
-import { createStore } from 'redux'
-import reducers from 'reducers'
+import React from 'react';
+import { createStore, applyMiddleware } from 'redux';
+import reducers from 'reducers';
+import reduxPromise from 'redux-promise';
 
 export default ({ children, initialState = {} }) => {
+  const store = createStore(
+    reducers, 
+    initialState, 
+    applyMiddleware(reduxPromise)
+  );
+
   return (
-    <Provider store={createStore(reducers, initialState)}>
+    <Provider store={store}>
       { children }
     </Provider>
   )
--- components/CommentBox.js
+++ components/CommentBox.js
@@ -23,13 +23,18 @@ class CommentBox extends Component {
 
   render(){
     return(
-      <form onSubmit={this.handleSubmit}>
+      <div>
+        <form onSubmit={this.handleSubmit}>
           <h4>Add a Comment</h4>
           <textarea onChange={this.handleChange} value={this.state.comment}></textarea>
           <div>
             <button>Submit Comment</button>
           </div>
         </form>
+        <button onClick={this.props.fetchComments}>Fetch Comments</button>
+      </div>
+        
+      
     )
   }
 }
