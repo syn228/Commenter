6b0887468e1ee734cef29d0b0a38290279fd4bdf
Aaron Na <syn228@nyu.edu>
2018-11-09 20:20:14 -0800

    set up exact routing for components

--- components/App.js
+++ components/App.js
@@ -1,12 +1,13 @@
 import React from 'react'
+import { Route } from 'react-router-dom'
 import CommentBox from 'components/CommentBox'
 import CommentList from 'components/CommentList'
 
 export default () => {
   return (
     <div>
-      <CommentBox />
-      <CommentList />
+      <Route path="post" compoent={CommentBox} />
+      <Route path="/" exact compoent={CommentList} />
     </div>
   )
 }
