3800f694fc9609c5d0e7536dc7690ea9359da517
Aaron Na <syn228@nyu.edu>
2018-11-11 21:40:25 -0800

    added tv4 to use json schema to validate state with middleware

--- Root.js
+++ Root.js
@@ -2,13 +2,14 @@ import { Provider } from 'react-redux'
 import React from 'react';
 import { createStore, applyMiddleware } from 'redux';
 import reducers from 'reducers';
+import stateValidator from 'middlewares/stateValidator'
 import asyncMiddleware from 'middlewares/async'
 
 export default ({ children, initialState = {} }) => {
   const store = createStore(
     reducers, 
     initialState, 
-    applyMiddleware(asyncMiddleware)
+    applyMiddleware(asyncMiddleware, stateValidator)
   );
 
   return (
---
+++ middlewares/stateValidator.js
@@ -0,0 +1,10 @@
+import stateSchema from './stateSchema'
+import tv4 from 'tv4'
+
+export default ({ dispatch, getState }) => next => action => {
+  next(action);
+
+  if(!tv4.validate(getState(), stateSchema)){
+    console.warn("Invalid state schema detected")
+  }
+}
